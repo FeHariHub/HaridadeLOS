c30852ec895de676f1a66019c25d3a9a4d0016df
HA_FeHari <alexandregomesap202@gmail.com>
2024-09-28 22:57:08 -0300

    Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -5,10 +5,10 @@ local games = {
 
 local gameUrl = games[game.PlaceId]
 
---> Invalid game check
+--> Jogo Não Suportado
 if (not gameUrl) then
-  return game:GetService("Players").LocalPlayer:Kick("This is not a game supported by Juice Hub.")
+  return game:GetService("Players").LocalPlayer:Kick("Oops! Este jogo ainda não é suportado. Estamos trabalhando para adicionar mais!)
 end
 
---> Load script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/incelsub/Juice-Hub/main" .. gameUrl))()
+--> Carregar Script!
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHari/HaridadeLOS/main/HaridadeTrial.lua"))()
