840d095f46e2cf9b7629fa1ae3e740b9b7e01ed4
FeHari <alexandregomesap202@gmail.com>
2024-10-23 22:39:46 -0300

    Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,14 +1,19 @@
---> Jogos
-local games = {
-  [3101667897] = "https://raw.githubusercontent.com/FeHariHub/HaridadeLOS/main/LendasDaVelocidade2.lua"
+  -- Executar
+local GameID = game.PlaceId
+
+-- Jogos
+local Supported_Games = {
+    LegendsOfSpeed_City = 3101667897,
+    LegendsOfSpeed_Space = 3232996272,
+    LegendsOfSpeed_Desert = 3276265788,
 }
 
-local gameUrl = games[game.PlaceId]
-
---> Jogo Não Suportado
-if (not gameUrl) then
-  return game:GetService("Players").LocalPlayer:Kick("Oops! Este jogo ainda não é suportado. Estamos trabalhando para adicionar mais!)
+-- Script
+if GameID == Supported_Games.LegendsOfSpeed_City
+or GameID == Supported_Games.LegendsOfSpeed_Space 
+or GameID == Supported_Games.LegendsOfSpeed_Desert then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/HaridadeLOS/main/LendasDaVelocidade2.lua"))()
+elseif GameID == Supported_Games.MuscleLegends then
+else
+    warn("Oops! Este jogo ainda não é suportado. Estamos trabalhando para adicionar mais!")
 end
-
---> Carregar Script!
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/HaridadeLOS/main/LendasDaVelocidade2.lua"))()
